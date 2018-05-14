20434c7660fb7d109568555351950f0b19a3c5a6
Pat Kelsh <pkelsh@gmail.com>
2018-05-14 15:11:22 -0400

    HUB-1219 fix merge error

--- lib/certify_email/resources/email.rb
+++ lib/certify_email/resources/email.rb
@@ -25,11 +25,7 @@ module CertifyEmail
 
     # helper for white listing parameters
     def self.email_safe_params(params)
-<<<<<<< HEAD
       permitted_keys = %w[id template recipient message subject inviter_name business_name program]
-=======
-      permitted_keys = %w[id template recipient message]
->>>>>>> develop
       symbolize_params(params.select { |key, _| permitted_keys.include? key.to_s })
     end
 
